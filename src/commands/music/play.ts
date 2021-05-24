229d46eba4d1094fe254284baed2269e61d11902
Cayce House <cayce@mobilemedics.com>
2021-05-24 23:27:11 +0000

    Linted code

--- src/commands/music/play.ts
+++ src/commands/music/play.ts
@@ -1,7 +1,6 @@
 import { ClientUser, CommandInteraction, VoiceConnection } from 'discord.js'
 import ytdl from 'ytdl-core-discord'
 import ytsr, { Video } from 'ytsr'
-import config from '../../config'
 
 export const Play = {
   name: 'play',
@@ -10,53 +9,50 @@ export const Play = {
     name: 'song',
     type: 'STRING',
     description: 'The song or URL to play from',
-    required: true,
+    required: true
   }],
-  async execute(interaction: CommandInteraction) {
+  async execute (interaction: CommandInteraction) {
     const song = String(interaction.options[0].value)
 
     const voice = interaction.member?.voice.channel
     if (voice === null) {
-      return interaction.reply('Please enter a voice channel first!', { ephemeral: true })
+      return await interaction.editReply('Please enter a voice channel first!')
     }
 
     const perms = voice?.permissionsFor((interaction.client.user as ClientUser))
     if (perms?.has('CONNECT') === false || perms?.has('SPEAK') === false) {
-      return interaction.reply('Unable to join the voice channel.', { ephemeral: true })
+      return await interaction.editReply('Unable to join the voice channel.')
     }
 
     var songURL: string
     if (ytdl.validateURL(song)) {
       songURL = song
     } else {
-      const filters1 = await ytsr.getFilters(song);
-      const filter1 = filters1.get('Type')?.get('Video');
-      if (filter1 && filter1.url) {
-        const searchResults = await ytsr(filter1.url);
+      const filters1 = await ytsr.getFilters(song)
+      const filter1 = filters1.get('Type')?.get('Video')
+      try {
+        const searchResults = await ytsr((filter1?.url as string))
         songURL = (searchResults.items[0] as Video).url
-      } else {
-        return interaction.reply('No results found.', { ephemeral: true })
+      } catch (error) {
+        console.error(`Play#execute#1 >> ${(error.stack as string)}`)
+        return await interaction.editReply('No results found.')
       }
     }
 
     try {
       voice.join().then(async (connection: VoiceConnection) => {
+        const dispatcher = connection.play(await ytdl(songURL, { filter: 'audioonly', quality: 'highestaudio' }), { type: 'opus' })
 
-        let stream = await ytdl(songURL, { filter: 'audioonly', quality: 'highestaudio' })
-
-        let dispatcher = connection.play(stream, { type: 'opus' })
-
-        dispatcher.on('end', () => voice.leave());
-      }).catch((error: { stack: string }) => {
-        console.error(`Play#execute >> ${error.stack}`)
-        return interaction.reply('An unexpected error has occured.', { ephemeral: true })
+        dispatcher.on('end', () => voice.leave())
+      }).catch(async (error: { stack: string }) => {
+        console.error(`Play#execute#2 >> ${error.stack}`)
+        return await interaction.editReply('An unexpected error has occured.')
       })
 
-      return interaction.reply(`Now Playing ${songURL}`)
+      return await interaction.editReply(`Now Playing ${songURL}`)
     } catch (error) {
-      console.error(`Play#execute >> ${error.stack}`);
-      return interaction.reply('An unexpected error has occured.', { ephemeral: true })
+      console.error(`Play#execute#3 >> ${(error.stack as string)}`)
+      return await interaction.editReply('An unexpected error has occured.')
     }
-
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -8,47 +8,57 @@ interface BaconClient extends Discord.Client {
 
 const client: BaconClient = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] })
 
-async function loadCommands() {
-  if (!client.commands) client.commands = new Collection();
+async function loadCommands (): Promise<void> {
+  if (client.commands == null) client.commands = new Collection()
 
   try {
     const keys = Object.keys(commands)
     for (const key of keys) {
-      client.commands?.set((commands as { [key: string]: any })[key].name, (commands as { [key: string]: any })[key]);
+      client.commands?.set((commands as { [key: string]: any })[key].name, (commands as { [key: string]: any })[key])
     }
 
-    console.info(`${client.commands?.array().length} commands loaded`);
+    console.info(`${client.commands?.array().length} commands loaded`)
   } catch (error) {
-    console.error(`index#loadCommands >> ${error.stack}`);
+    console.error(`index#loadCommands >> ${(error.stack as string)}`)
   }
 
   const remote = client.guilds.cache.get('727984268793479301')
 
-  await remote?.commands.set(client.commands!.array());
+  await remote?.commands.set(client.commands.array())
 
   console.info(
     `${client.commands?.array().length} interactions loaded`
-  );
+  )
 }
 
-client.on('interaction', async interaction => {
-  if (!interaction.isCommand()) return;
+client.on('interaction', interaction => {
+  if (!interaction.isCommand()) return
+
+  interaction.defer(false).catch((error) => {
+    console.error(`An error occured while deferring interaction. >> ${(error.stack as string)}`)
+  })
 
   const command = (interaction.client as BaconClient).commands?.get(interaction.commandName) ||
-    (interaction.client as BaconClient).commands?.find(cmd => cmd.aliases && cmd.aliases.includes(interaction.commandName))
+    (interaction.client as BaconClient).commands?.find(cmd => cmd.aliases.includes(interaction.commandName))
 
   try {
     command.execute(interaction)
   } catch (error) {
     console.error(error)
-    interaction.reply('there was an error trying to execute that command!', { ephemeral: true })
+    interaction.editReply('there was an error trying to execute that command!').catch((error) => {
+      console.error(`An error occured while editing reply to interaction. >> ${(error.stack as string)}`)
+    })
   }
-});
+})
 
-client.once('ready', async () => {
-  await loadCommands()
+client.once('ready', () => {
+  loadCommands().catch((error) => {
+    console.error(`An error occured while loading commands. >> ${(error.stack as string)}`)
+  })
 
   console.log('Ready!')
 })
 
-client.login(config.token)
+client.login(config.token).catch((error) => {
+  console.error(`An error occured while logging in. >> ${(error.stack as string)}`)
+})
