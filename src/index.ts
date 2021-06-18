602bd4b8478b570152ee62e9f450da8e308dfb59
Cayce House <cayce@mobilemedics.com>
2021-06-18 15:46:00 +0000

    Removed aws autostop features

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,6 @@ import { Collection, Intents } from 'discord.js'
 import config from './config'
 import * as commands from './commands'
 import { Bot } from './bot'
-import axios from 'axios'
-import { Credentials, Lambda } from 'aws-sdk'
-import { Server } from '@fabricio-191/valve-server-query'
 
 const client = new Bot({
   intents: [Intents.FLAGS.GUILDS,
@@ -67,64 +64,3 @@ client.once('ready', () => {
 client.login(config.token).catch((error) => {
   console.error(`An error occured while logging in. >> ${(error.stack as string)}`)
 })
-
-var previousCountMc = 1;
-setInterval(() => {
-  axios.get(`https://api.mcsrvstat.us/2/${config.minecraftIP}`).then(function (response) {
-    if (response.data.online === true && response.data.players.online === 0 && previousCountMc === 0) {
-      const credentials = new Credentials(config.awsAccessKeyId, config.awsSecretAccessKey)
-
-      const lambda = new Lambda({ region: config.awsRegion, credentials })
-
-      var params = {
-        FunctionName: config.awsMCFunctionName,
-        Payload: '{ "status": "Stop" }'
-      }
-      lambda.invoke(params, function (err, data) {
-        if (err) {
-          console.log(err, err.stack)
-        } else {
-          console.log(data)
-        }
-      })
-    } else if(response.data.online === false) {
-      previousCountMc = 1;
-    } else {
-      previousCountMc = response.data.players.online;
-    }
-  }).catch(function (error) {
-    console.log(error)
-  })
-}, 900000);
-
-var previousCountVh = 0;
-setInterval(async () => {
-  Server.getInfo({
-    ip: config.valheimIP,
-    port: 2457,
-  })
-    .then((server: { players: { online: any } }) => {
-      if (server.players.online === 0 && previousCountVh === 0) {
-        const credentials = new Credentials(config.awsAccessKeyId, config.awsSecretAccessKey)
-
-        const lambda = new Lambda({ region: config.awsRegion, credentials })
-
-        var params = {
-          FunctionName: config.awsValheimFunctionName,
-          Payload: '{ "status": "Stop" }'
-        }
-        lambda.invoke(params, function (err, data) {
-          if (err) {
-            console.log(err, err.stack)
-          } else {
-            console.log(data)
-          }
-        })
-      } else {
-        previousCountVh = server.players.online;
-      }
-    })
-    .catch(() => {
-      previousCountVh = 1
-    });  
-}, 900000);
