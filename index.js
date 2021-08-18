63e2487b2fecb350dc2b7148e29ede53c14f3e21
Cayce House <cayce@mobilemedics.com>
2021-08-18 00:57:26 -0400

    Switch to process env

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const fs = require('fs')
 const { Client, Collection, Intents } = require('discord.js')
-const { token } = require('./config.json')
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 client.commands = new Collection()
@@ -29,4 +28,4 @@ client.on('interactionCreate', async interaction => {
   }
 })
 
-client.login(token)
+client.login(process.env.TOKEN)
--- register.js
+++ register.js
@@ -1,6 +1,5 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { token, guildId, clientId } = require('./config.json');
 const fs = require('fs');
 
 const commands = [];
@@ -11,14 +10,14 @@ for (const file of commandFiles) {
 	commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: '9' }).setToken(token);
+const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 
 (async () => {
 	try {
 		console.log('Started refreshing application (/) commands.');
 
 		await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
+			Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
 			{ body: commands },
 		);
 
